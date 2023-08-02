1fec9a8a88e5209c5e93a7c38a70d9f126e2ce96
MatB1988 <bassomatias88@gmail.com>
2023-08-02 20:47:31 -0300

    .

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # Librerias
 import numpy as np
 import pandas as pd
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi_versioning import VersionedFastAPI, version
+from fastapi.templating import Jinja2Templates
 import os
 import fastparquet
 
 app = FastAPI(title="Bienvenido a Mi API")
-dir_actual = os.getcwd()+'/Dataset/'
 
 # Ruta raíz para obtener el logotipo
 @app.get("/logo1.png")
@@ -19,19 +19,25 @@ async def get_logo():
 app_version = "1.0.0"
 app = VersionedFastAPI(app, version=app_version)
 
-# Endpoint de ejemplo con versión
+# Ruta para mostrar el formulario con la lista desplegable
+templates = Jinja2Templates(directory="templates")
+
 @version(1)
 @app.get('/peliculas_idioma/{idioma}')
-def peliculas_idioma_v1(idioma: str):
-    # Tu código aquí para la versión 1
-    df = pd.read_parquet(dir_actual + 'df_idioma_agrupado')
-    cantidad_peliculas = df[df['original_language'] == idioma]['cantidad_peliculas'].sum()
-    return {'mensaje': f"Versión 1 - La cantidad de peliculas estrenadas en idioma {idioma} es de {cantidad_peliculas}"}
+def peliculas_idioma_v1(request: Request, idioma: str):
+    idiomas_completos = [
+        'Abkhazian', 'Afrikaans', 'Amharic', 'Arabic', 'Aymara', 'Bulgarian', 'Bambara', 'Bengali', 'Tibetan', 'Bosnian',
+        'Catalan', 'Chinese', 'Czech', 'Welsh', 'Danish', 'German', 'Greek', 'English', 'Esperanto', 'Spanish', 'Estonian',
+        'Basque', 'Persian', 'Finnish', 'French', 'Frisian', 'Galician', 'Hebrew', 'Hindi', 'Croatian', 'Hungarian',
+        'Armenian', 'Indonesian', 'Icelandic', 'Italian', 'Inuktitut', 'Japanese', 'Javanese', 'Georgian', 'Kazakh',
+        'Kannada', 'Korean', 'Kurdish', 'Kyrgyz', 'Latin', 'Luxembourgish', 'Lao', 'Lithuanian', 'Latvian', 'Macedonian',
+        'Malayalam', 'Mongolian', 'Marathi', 'Malay', 'Maltese', 'Norwegian Bokmål', 'Nepali', 'Dutch', 'Norwegian',
+        'Punjabi', 'Polish', 'Pashto', 'Portuguese', 'Quechua', 'Romanian', 'Russian', 'Kinyarwanda', 'Serbo-Croatian',
+        'Sinhala', 'Slovak', 'Slovenian', 'Samoan', 'Albanian', 'Serbian', 'Swedish', 'Tamil', 'Telugu', 'Tajik', 'Thai',
+        'Tagalog', 'Turkish', 'Ukrainian', 'Urdu', 'Uzbek', 'Vietnamese', 'Wolof', 'Unknown', 'Chinese', 'Zulu'
+    ]
 
-@version(2)
-@app.get('/peliculas_idioma/{idioma}')
-def peliculas_idioma_v2(idioma: str):
-    # Tu código aquí para la versión 2
-    df = pd.read_parquet(dir_actual + 'df_idioma_agrupado')
-    cantidad_peliculas = df[df['original_language'] == idioma]['cantidad_peliculas'].sum()
-    return {'mensaje': f"Versión 2 - La cantidad de peliculas estrenadas en idioma {idioma} es de {cantidad_peliculas}"}
+    return templates.TemplateResponse(
+        "peliculas_idioma.html",
+        {"request": request, "idioma": idioma, "idiomas_completos": idiomas_completos}
+    )
